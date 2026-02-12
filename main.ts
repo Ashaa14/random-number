37f2bdf57bde0ab4492436d1a8ecea3bf6cc14fe
Ashaa14 <notarlene1014@gmail.com>
2026-02-12 07:28:02 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+function SetDicePattern () {
+    list = [
+    images.createImage(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . . . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . . # . .
+        . # . # .
+        . . . . .
+        `),
+    images.createImage(`
+        . . . . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . . . .
+        `)
+    ]
+}
+function RollDice () {
+    list[randint(0, 5)].showImage(0)
+}
+input.onGesture(Gesture.Shake, function () {
+    DiceValue = randint(0, 6)
+    basic.showNumber(DiceValue)
+})
+let DiceValue = 0
+let list: Image[] = []
+SetDicePattern()
+RollDice()
